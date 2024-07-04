bca1d3935d8620d1420d11ed561b7d652dc12521
konstrukt24 <kkltzda@gmail.com>
2024-07-05 00:32:09 +0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className="videoContainer">
-      <video autoPlay loop muted playsinline>
+      <video autoplay loop muted playsinline>
         <source src="/background.MP4" type="video/mp4" />  
       </video>
     </div>
