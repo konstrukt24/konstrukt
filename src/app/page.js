bf76ff5570889355e65c05a2cb49dee7f9940d42
konstrukt24 <kkltzda@gmail.com>
2024-07-05 00:39:34 +0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
-import React from "react";
+"use client"
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const video = document.getElementById('bgVideo');
+
+    video.setAttribute('controls', false);
+
+    video.addEventListener('click', function() {
+      if (video.hasAttribute('controls')) {
+        video.removeAttribute('controls');
+      } else {
+        video.setAttribute('controls', false);
+      }
+    });
+
+    return () => {
+      video.removeEventListener('click');
+    };
+  }, []);
+  
   return (
     <div className="videoContainer">
-      <video autoPlay loop muted playsInline>
+      <video id="bgVideo" autoPlay loop muted playsInline>
         <source src="/background.MP4" type="video/mp4" />  
       </video>
     </div>
