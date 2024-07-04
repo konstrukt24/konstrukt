61ebf723b9cf913a40df067755216f5f97028425
konstrukt24 <kkltzda@gmail.com>
2024-07-05 00:09:29 +0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 export default function Home() {
   return (
-    <div className="page-content">
-
+    <div className="videoContainer">
+      <video autoPlay loop muted src="/video/background.MP4" type="video/mp4" />
     </div>
   );
 }
