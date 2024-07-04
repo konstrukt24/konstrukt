8d574021aa236c5a16d1a3e82e64b6f7d40d1877
konstrukt24 <kkltzda@gmail.com>
2024-07-04 23:29:39 +0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,6 @@ export default function Home() {
       <video autoPlay loop muted>
         <source src="/video/background.mp4" type="video/mp4" />
       </video>
-
-      <audio controls>
-        <source src="/audio/background.mp3" type="audio/mpeg" />
-      </audio>
     </div>
   );
 }
